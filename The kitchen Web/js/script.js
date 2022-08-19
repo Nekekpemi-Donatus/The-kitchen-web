748652cee61e0f9279ee85cce0e769bc36d51871
Nekekpemi Donatus <111020780+Nekekpemi-Donatus@users.noreply.github.com>
2022-08-19 21:09:54 +0100

    Add files via upload

---
+++ The kitchen Web/js/script.js
@@ -0,0 +1,59 @@
+const sectionHeaderEl = document.querySelector(".header-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeaderEl);
+
+///////////////////////////////////////////////////
+// Opening / Closing the Menu
+
+const headerEl = document.querySelector(".header-section");
+const btnEl = document.querySelector(".btn-mobile-nav");
+
+btnEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
